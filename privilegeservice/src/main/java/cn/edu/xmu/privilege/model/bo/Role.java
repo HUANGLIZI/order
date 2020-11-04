34a25ff24224653142938d0d18776eb4650d96df
Weice Wang <24320182203281@stu.xmu.edu.cn>
2020-11-04 20:15:45 +0800

    auth008: 查询角色, 新增角色, 删除角色,修改角色信息

--- privilegeservice/src/main/java/cn/edu/xmu/privilege/model/bo/Role.java
+++ privilegeservice/src/main/java/cn/edu/xmu/privilege/model/bo/Role.java
@@ -1,7 +1,12 @@
 package cn.edu.xmu.privilege.model.bo;
 
+import cn.edu.xmu.ooad.model.VoObject;
+import cn.edu.xmu.privilege.model.po.RolePo;
+import cn.edu.xmu.privilege.model.vo.RoleRetVo;
+import cn.edu.xmu.privilege.model.vo.RoleVo;
 import lombok.Data;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
 /**
@@ -10,12 +15,68 @@ import java.time.LocalDateTime;
  * @date Created in 2020/11/3 11:48
  **/
 @Data
-public class Role {
-
+public class Role implements VoObject, Serializable {
     private Long id;
     private String name;
     private Long creatorId;
     private String describe;
     private LocalDateTime gmtCreate;
     private LocalDateTime gmtModified;
+
+    public Role() {
+    }
+
+    /**
+     * 构造函数     *
+     * @param po 用PO构造
+     * @return Role
+     */
+    public Role(RolePo po) {
+        this.id = po.getId();
+        this.name = po.getName();
+        this.creatorId = po.getCreatorId();
+        this.describe = po.getDescr();
+        this.gmtCreate = po.getGmtCreate();
+        this.gmtModified = po.getGmtModified();
+    }
+
+    /**
+     * 生成RoleRetVo对象作为返回前端
+     * @return Object
+     */
+    @Override
+    public Object createVo() {
+        return new RoleRetVo(this);
+    }
+
+    /**
+     * 用vo对象创建更新po对象
+     * @param vo vo对象
+     * @return po对象
+     */
+    public RolePo createUpdatePo(RoleVo vo){
+        RolePo po = new RolePo();
+        po.setId(this.getId());
+        po.setName(vo.getName());
+        po.setCreatorId(null);
+        po.setDescr(vo.getDescr());
+        po.setGmtCreate(null);
+        po.setGmtModified(LocalDateTime.now());
+        return po;
+    }
+
+    /**
+     * 用bo对象创建更新po对象
+     * @return RolePo
+     */
+    public RolePo gotRolePo() {
+        RolePo po = new RolePo();
+        po.setId(this.getId());
+        po.setName(this.getName());
+        po.setCreatorId(this.getCreatorId());
+        po.setDescr(this.getDescribe());
+        po.setGmtCreate(this.getGmtCreate());
+        po.setGmtModified(this.getGmtModified());
+        return po;
+    }
 }
